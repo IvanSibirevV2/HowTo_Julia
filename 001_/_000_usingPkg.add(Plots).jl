998cc5f34db3eff99f849a664ac8e779d18ae766
qwe <qwe>
2021-01-13 11:03:44 +0300

    ***

---
+++ 001_/_000_usingPkg.add(Plots).jl
@@ -0,0 +1,5 @@
+#using Pkg.add(Plots)
+using Pkg
+Pkg.add("Plots")
+Pkg.add("LinearAlgebra")
+Pkg.add("Interact")
---
+++ 001_/_001_.jl
@@ -0,0 +1,3 @@
+using LinearAlgebra, Plots, Interact
+x = 1:10; y = rand(10); # These are the plotting data 
+plot(x,y, label="my label")
