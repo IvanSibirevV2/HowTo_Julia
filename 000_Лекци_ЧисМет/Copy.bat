211fffe5d5a56af37ca80dd16f7c2d79e9b09a65
qwe <qwe>
2021-07-15 08:07:57 +0300

    ******

---
+++ 000_Лекци_ЧисМет/Copy.bat
@@ -0,0 +1,12 @@
+chcp 1251
+@Set Pach1=C:\Users\SibNout2020\000_Лекци_ЧисМет\
+@Set Pach2=C:\D\YandexDisk\Преподавание2021\Обменка\ЧисленныеМетоды\000_Лекци_ЧисМет\
+::copy "%Pach1%2ИСИП-319.xls" "%Pach2%2ИСИП-319.xls"
+@copy "%Pach1%*.ipynb" "%Pach2%"
+@copy "%Pach1%*.html" "%Pach2%"
+@Set Pach1=C:\Users\SibNout2020\000_Лекци_ЧисМет\
+@Set Pach2=C:\D\Git_Hub\HowTo_Julia\000_Лекци_ЧисМет\
+::copy "%Pach1%2ИСИП-319.xls" "%Pach2%2ИСИП-319.xls"
+@copy "%Pach1%*.ipynb" "%Pach2%"
+@copy "%Pach1%*.html" "%Pach2%"
+TIMEOUT /T 5
---
+++ 002_Лекци_Julia_/Copy.bat
@@ -0,0 +1,13 @@
+chcp 1251
+@Set Pach1=C:\Users\SibNout2020\002_Лекци_Julia_\
+@Set Pach2=C:\D\YandexDisk\Преподавание2021\Обменка\ЧисленныеМетоды\Julia\
+::copy "%Pach1%2ИСИП-319.xls" "%Pach2%2ИСИП-319.xls"
+@copy "%Pach1%*.ipynb" "%Pach2%"
+@copy "%Pach1%*.html" "%Pach2%"
+@Set Pach1=C:\Users\SibNout2020\002_Лекци_Julia_\
+@Set Pach2=C:\D\Git_Hub\HowTo_Julia\002_Лекци_Julia_\
+::copy "%Pach1%2ИСИП-319.xls" "%Pach2%2ИСИП-319.xls"
+@copy "%Pach1%*.ipynb" "%Pach2%"
+@copy "%Pach1%*.html" "%Pach2%"
+::pause
+TIMEOUT /T 5
